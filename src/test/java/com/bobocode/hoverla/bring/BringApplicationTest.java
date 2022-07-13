71275b5a29e018013d861824746830175729eb73
Danyl Miniailo <danyl.miniailo@care.com>
2022-07-13 14:19:46 +0300

    ci fails on test failure

---
+++ src/test/java/com/bobocode/hoverla/bring/BringApplicationTest.java
@@ -0,0 +1,13 @@
+package com.bobocode.hoverla.bring;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BringApplicationTest {
+
+    @Test
+    void m() {
+        assertTrue(false);
+    }
+}
