4ff71f1745b5b4cdb4b579af16bc40e824b12bc1
Nazar Koval <nazarmarmeladka228@gmail.com>
2022-07-28 23:08:20 +0300

    enhance BeanDefinition interface, BeanInitializer, ConfigBasedBeanDefinition, adjust tests

--- src/main/java/com/bobocode/hoverla/bring/context/BeanDefinition.java
+++ src/main/java/com/bobocode/hoverla/bring/context/BeanDefinition.java
@@ -1,42 +1,65 @@
 package com.bobocode.hoverla.bring.context;
 
+import com.bobocode.hoverla.bring.annotation.Bean;
+
 import java.util.Map;
 
 /**
  * Describes a bean instance, which has name, type, dependencies and further information supplied by
  * concrete implementations.
  *
+ * @see Bean @Bean
  * @see ConfigBasedBeanDefinition
  */
 public interface BeanDefinition {
 
     /**
-     * Returns bean name
-     * @return bean name
+     * Method for getting name of current {@link BeanDefinition}.
+     *
+     * @return name of current {@link BeanDefinition}
      */
     String name();
 
     /**
-     * Returns bean instance type
-     * @return bean instance type
+     * Method for getting type of current {@link BeanDefinition}.
+     *
+     * @return type of current {@link BeanDefinition}
      */
     Class<?> type();
 
     /**
-     * Returns bean dependencies that are required for its instantiation in a format of Map where key
-     * is dependency's name and value is dependencies type
-     * @return bean dependencies map
+     * Returns dependencies of current {@link BeanDefinition} that are required for its instantiation in a format of {@link Map}.
+     * <p>Key in this map is a {@link String} object that represents name of dependent {@link BeanDefinition}.</p>
+     * <p>Value is this map is a {@link Class} object that represents type of dependent {@link BeanDefinition}.</p>
+     *
+     * @return map of names and types of dependent {@link BeanDefinition}s.
      */
     Map<String, Class<?>> dependencies();
 
     /**
-     * Instantiates bean using all necessary information encapsulated within.
+     * Handy method to check whether a bean instance has already been created from current {@link BeanDefinition}.
      *
-     * <p>Requires bean definition which are treated as dependencies to be passed as arguments.</p>
-     * <p>If arguments don't match the actual required dependencies, the exception will be thrown.</p>
-     *
-     * @param dependencies bean definitions that are treated as required dependencies for this bean instantiation
-     * @return bean instance
+     * @return {@code true} if bean was instantiated, {@code false} otherwise.
      */
-    Object instance(BeanDefinition... dependencies);
+    boolean isInstantiated();
+
+    /**
+     * Instantiates a bean from current {@link BeanDefinition} using all necessary information encapsulated within.
+     *
+     * <p>Requires {@link BeanDefinition}s that are treated as dependencies to be passed as arguments.</p>
+     * <p>If arguments don't match the actual required dependencies, an exception will be thrown.</p>
+     *
+     * @param dependencies {@link BeanDefinition} objects that are treated as required dependencies for
+     *                     instantiating a bean from current {@link BeanDefinition}
+     */
+    void instantiate(BeanDefinition... dependencies);
+
+    /**
+     * Method for getting a created bean instance of current {@link BeanDefinition}.
+     *
+     * @return bean instance of current {@link BeanDefinition}.
+     * @throws NullPointerException when instance of current {@link BeanDefinition} is null.
+     */
+    Object getInstance();
+
 }
