54d95388eda858c07301e250c93adf49b611fe2c
nadya-gainutsa <nadya.gainutsa@gmail.com>
2022-07-29 17:19:13 +0300

    fix sonar issue

--- src/main/java/com/bobocode/hoverla/bring/BringApplication.java
+++ src/main/java/com/bobocode/hoverla/bring/BringApplication.java
@@ -107,8 +107,8 @@ public class BringApplication {
             return this;
         }
 
-        @SuppressWarnings(value = "Implementation does not contain the vulnerabilities listed in this rule" +
-                "https://rules.sonarsource.com/java/RSPEC-4792")
+        @SuppressWarnings(value = {"java:S4792", "Implementation does not contain the vulnerabilities listed in this rule" +
+                "https://rules.sonarsource.com/java/RSPEC-4792"})
         public ApplicationContext build() {
             Logger logger = (Logger) LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
             logger.setLevel(logLevel != null ? logLevel : Level.INFO);
